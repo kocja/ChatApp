e7fbec1c9bb99a7b4a43e8b1fc45912113312f7f
kocja <69508289+kocja@users.noreply.github.com>
2021-04-28 17:16:22 +0200

    timertask implemented

--- src/main/java/ch/admin/seco/chatserver/Scheduler.java
+++ src/main/java/ch/admin/seco/chatserver/Scheduler.java
@@ -1,19 +1,25 @@
 package ch.admin.seco.chatserver;
 
+import ch.admin.seco.chatserver.service.UserService;
+
+import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class Scheduler  {
+public class Scheduler extends TimerTask {
 
     private final Timer timer;
     private final int interval = 60;
+    private final UserService userService;
 
-
-    public Scheduler(Timer timer) {
+    public Scheduler(Timer timer, UserService userService) {
         this.timer = timer;
+        this.userService = userService;
     }
 
-
-
-
+    @Override
+    public void run() {
+        userService.updateStatus();
+        System.out.println("User updated at: " + new Date());
+    }
 }
